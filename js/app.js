c71bfe431aa7a68d0f020e2a1142d663ba4d685c
Barbir Tudor <barbirdimitris@gmail.com>
2016-09-10 12:29:56 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,21 +1,8 @@
 var app = angular.module("MyApp", []);
-app.controller("TestCtrl",['$scope', function($scope){
-		$scope.name='Tudor';
+app.controller("TestCtrl",['$scope', '$http', function($scope, $http){     //declaratia acestui controller ,TestCtrl-functie
+	$http.get("https://jsonplaceholder.typicode.com/users").then(function(response){
+		console.log(response.data);
+		$scope.data=response.data;
+	})									//declaratia serviciilor					
 
-		$scope.addName = function(){
-			console.log("Button Clicked");
-			console.log($scope.firstName);
-
-			$scope.names.push($scope.firstName);
-			$scope.firstName="";
-		}
-
-		$scope.delete=function(index){
-			console.log(index);
-			$scope.names.splice(index, 1);
-		}
-
-		$scope.names = [
-			'carmen', 'ion', 'barbara', 'streissand'
-		]
 }]);
